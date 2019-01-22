2a8fffdd2f64cee765ad21326f87a05f7d370dcd
megzcalvert <calvertmegz@gmail.com>
2019-01-22 12:33:15 -0600

    Looking at Kansas

--- TT20190122.R
+++ TT20190122.R
@@ -9,3 +9,33 @@ str(rawData)
 
 unique(rawData$state)
 unique(rawData$year)
+
+##### Examine individuals who are in prison pre-trial
+preTrial<- rawData %>% 
+  select(year,state,total_pop,urbanicity,region,division,county_name,
+         ends_with("crime"),ends_with("pretrial")) %>%
+  filter(total_jail_pretrial > 0)
+
+# what is the pre-trial population doing in the different regions by year
+ggplot(preTrial, aes(year,total_jail_pretrial)) +
+  geom_point() +
+  facet_wrap("region", scales = "free") +
+  theme_bw()
+
+# I live in the midwest so lets look at the midwest and how much difference
+# the urban area makes
+preTrial %>% 
+  filter(region == "Midwest") %>%
+  ggplot(aes(year,total_jail_pretrial, color=urbanicity)) + 
+  geom_point(aes(shape=division)) +
+  facet_wrap("state", scales = "free") +
+  theme_bw()
+
+# Deeper into Kansas we go...
+preTrial %>% 
+  filter(state == "KS") %>%
+  ggplot(aes(year,total_jail_pretrial, color=urbanicity)) + 
+  geom_point(aes(shape=division)) +
+  facet_wrap("county_name", scales = "free") +
+  theme_bw()
+#not really helpful
