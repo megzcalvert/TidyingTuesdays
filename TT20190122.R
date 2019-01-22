6ed44d3baffaef03c5c8a7a07eb43e12c7add5f7
megzcalvert <calvertmegz@gmail.com>
2019-01-22 11:49:11 -0600

    Read data in

--- TT20190122.R
+++ TT20190122.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+library(RCurl)
+
+x <- getURL("https://raw.githubusercontent.com/vera-institute/incarceration_trends/master/incarceration_trends.csv")
+rawData<- read.csv(text = x)
+colnames(rawData)
+
+str(rawData)
+
+unique(rawData$state)
+unique(rawData$year)
